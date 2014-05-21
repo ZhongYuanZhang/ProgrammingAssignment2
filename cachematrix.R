57e31fec26e29bb66ddc94ab0eb74daae9b5cf85
Zhong-Yuan Zhang <zhzhyuan@amss.ac.cn>
2014-05-21 17:52:52 +0800

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,18 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix = function(x = matrix()) {
+  Inver = NULL
+  set = function(y){
+    x <<- y
+    Inver <<- NULL
+  }
+  get = function() x
+  setSolve = function(Inver2) Inver <<- Inver2
+  getSolve = function() Inver
+  return(list(set = set, get = get,
+              setSolve = setSolve, 
+              getSolve = getSolve))
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  Inver = x$getSolve()
+  if (!is.null(Inver)){
+    message('getting cached data')
+    return(Inver)
+  }
+  matrix = x$get()
+  Inver = solve(matrix,...)
+  x$setSolve(Inver)
+  Inver
 }
